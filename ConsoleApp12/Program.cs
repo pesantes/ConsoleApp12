281c0f5cd64b9a83e6dc19354783bbd9d3d36661
Fernando Pesantes <pesantes@usf.edu>
2019-01-31 06:31:34 -0500

    Version 1

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+Author: Fernando Pesantes
+Date: 1/31/2019
+Description: Methods Class Project for ISM 4300
+
+ */
+
+using System;
 
 namespace ConsoleApp12
 {
@@ -6,7 +13,28 @@ namespace ConsoleApp12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your name: ");
+            try
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine("Hello " + input);               
+                Console.ReadKey(true);
+
+            } // End of try
+            catch
+            {
+
+            } // End of catch
+        } // End of main
+
+        private string ShowName(string get_name)
+        {
+            string display_name = get_name;
+            return get_name;
+           
+            
+            
         }
+
     }
 }
